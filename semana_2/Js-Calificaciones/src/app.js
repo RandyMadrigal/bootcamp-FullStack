2e50f4eff8b63879263ad53056e6ee3cacabfefe
Randy Madrigal <randymadrigalx12@gmail.com>
2024-08-16 19:10:03 -0400

    add design 70%

---
+++ semana_2/Js-Calificaciones/src/app.js
@@ -0,0 +1,12 @@
+const calificaciones = [
+  { nombre: "Juan", calificacion: 85 },
+  { nombre: "Ana", calificacion: 92 },
+  { nombre: "Pedro", calificacion: 78 },
+  { nombre: "Lucía", calificacion: 88 },
+  { nombre: "Carlos", calificacion: 76 },
+  { nombre: "María", calificacion: 95 },
+  { nombre: "Luis", calificacion: 84 },
+  { nombre: "Laura", calificacion: 91 },
+  { nombre: "Jorge", calificacion: 80 },
+  { nombre: "Sofía", calificacion: 89 },
+];
