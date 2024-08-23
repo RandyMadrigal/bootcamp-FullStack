bc28d3d30ac8584c4736f45291a6e9a190cd6711
Randy Madrigal <randymadrigalx12@gmail.com>
2024-08-22 20:50:21 -0400

    first commit POO

---
+++ semana_3/POO/src/app.js
@@ -0,0 +1,56 @@
+class Persona {
+  constructor(nombre, edad, ...intereses) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.intereses = intereses;
+  }
+
+  AñadirInteres(...str) {
+    console.log(...str);
+
+    this.intereses.push(...str);
+  }
+
+  EliminarInteres(str) {
+    const index = this.intereses.findIndex(
+      (i) => i.toUpperCase() === str.toUpperCase()
+    );
+    console.log(index);
+    if (index < 0) {
+      console.log("no data");
+      return;
+    }
+    this.intereses.splice(index, 1);
+  }
+
+  MostrarIntereses() {
+    console.log(`
+      **************************************
+      Los intereses de ${this.nombre} son:
+      ${this.intereses}
+      ************************************** 
+       `);
+  }
+
+  MostrarInformacion() {
+    console.log(`
+      *************************************
+      Nombre: ${this.nombre}             
+      Edad: ${this.edad}                 
+      Intereses: ${this.intereses}       
+      *************************************`);
+  }
+}
+
+const randy = new Persona(
+  "Randy Madrigal",
+  18,
+  "Software",
+  "Gym",
+  "Basketball"
+);
+
+randy.AñadirInteres("Android");
+randy.EliminarInteres("basketball");
+randy.MostrarIntereses();
+randy.MostrarInformacion();
