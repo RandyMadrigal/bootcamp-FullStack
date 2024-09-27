5b1270c045d08eab9c65d47ab2bf8234f594af80
Randy Madrigal <randymadrigalx12@gmail.com>
2024-09-26 23:21:10 -0400

    First commit ts-practice

---
+++ typescript/practice_1/app.ts
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const DEFAULT_VALUE : number= 0
+
+    const btnSumar = document.getElementById("btnSumar") as HTMLButtonElement;
+    const resultado = document.getElementById("resultado") as HTMLParagraphElement 
+
+    btnSumar.addEventListener("click", () : void  =>{
+       resultado.textContent =  sumar().toString();
+       cleanInput();
+    })
+
+    const sumar = () : number  => {
+        const numero1Element = document.getElementById("num1") as HTMLInputElement;
+        const numero2Element = document.getElementById("num2")  as HTMLInputElement;
+        let result : number;
+
+       isValidate(numero1Element.value,numero2Element.value) ? 
+       result = parseInt(numero1Element.value) + parseInt(numero2Element.value) 
+       : result = DEFAULT_VALUE;
+
+       return result     
+    }
+
+    const isValidate = (num1 : string , num2 : string) : boolean => {
+        let validate : boolean = true;
+
+        if(num1.length === 0 || num2.length === 0){
+            validate = false
+            alert("Completar todos los campos.")
+        };
+
+        return validate;
+    }
+
+    const cleanInput = () : void => {
+        const clean = document.querySelectorAll("input") 
+        console.log(clean)
+        clean.forEach( el => {
+            el.value = "";
+        })
+    }
+
+})
+
+
+
+
+
---
+++ typescript/practice_1/dist/app.js
@@ -0,0 +1,35 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const DEFAULT_VALUE = 0;
+    const btnSumar = document.getElementById("btnSumar");
+    const resultado = document.getElementById("resultado");
+    btnSumar.addEventListener("click", () => {
+        resultado.textContent = sumar().toString();
+        cleanInput();
+    });
+    const sumar = () => {
+        const numero1Element = document.getElementById("num1");
+        const numero2Element = document.getElementById("num2");
+        let result;
+        isValidate(numero1Element.value, numero2Element.value) ?
+            result = parseInt(numero1Element.value) + parseInt(numero2Element.value)
+            : result = DEFAULT_VALUE;
+        return result;
+    };
+    const isValidate = (num1, num2) => {
+        let validate = true;
+        if (num1.length === 0 || num2.length === 0) {
+            validate = false;
+            alert("Completar todos los campos.");
+        }
+        ;
+        return validate;
+    };
+    const cleanInput = () => {
+        const clean = document.querySelectorAll("input");
+        console.log(clean);
+        clean.forEach(el => {
+            el.value = "";
+        });
+    };
+});
